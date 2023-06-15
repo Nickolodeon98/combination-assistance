4cfe07a722c206fa24464393851a15b76c088f4c
Nickolodeon98 <sjeon0730@gmail.com>
2023-06-15 20:18:19 +0900

    feat: add a new project

---
+++ src/main/java/com/example/combproject/CombProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.combproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CombProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CombProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/combproject/CombProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.combproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CombProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
